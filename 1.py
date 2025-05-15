c339314e4924f40a76e70401e7f874ad739ed1a3
TiranBAS <mypochta2022new@gmail.com>
2025-05-15 19:46:40 +0400

    гг

--- 1.py
+++ 1.py
@@ -1,30 +1,30 @@
 import pygame
 import sys
-
+from threading import Timer
 pygame.init()
 
-width, height = 800, 750
+width, height = 900, 700
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('kk')
 
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
-green = (0, 255, 0)
-yellow = (255, 255, 0)
+green = (0, 255,0)
+blue = (33, 33, 33)
 
 cell_size= 100
 
 # Урaвни(1-стена, 2-бокс, 3-дверь, 4-перс)
 levels = [
     [
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 3, 0, 0, 3, 0, 3],
-        [1, 0, 2, 2, 0, 0, 1],
-        [1, 0, 0, 4, 0, 3, 1],
-        [1, 0, 0, 0, 2, 0, 1],
-        [1, 2, 0, 2, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1]
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 4, 2, 0, 0, 2, 0, 0, 3],
+        [1, 0, 0, 0, 2, 0, 0, 0, 1],
+        [1, 0, 0, 2, 0, 0, 2, 0, 1],
+        [1, 0, 2, 0, 0, 2, 0, 0, 1],
+        [3, 0, 0, 0, 2, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1]
     ]
 ]
 
@@ -34,6 +34,28 @@ boxes = []
 key = []
 walls = []
 
+font = pygame.font.Font(None, 50)# None - Название шрифта, 20 - размер
+timerText = font.render("00:00", True, (255, 0, 0))
+
+timerSecond = 0
+timerMinute = 0
+
+
+def increaseTime():
+    global timerSecond, timerMinute, running
+    if timerSecond == 59:
+        timerSecond = 0
+        timerMinute += 1
+    else:
+        timerSecond += 1
+    timer = Timer(1, increaseTime)
+    if not running:
+        timer.join()
+    else:
+        timer.start()
+
+timer = Timer(1, increaseTime)
+timer.start()
 
 def Roon(level):
     global player, boxes, key, walls
@@ -62,28 +84,48 @@ running = True
 
 
 def draw_game():
-    screen.fill(white)
+    screen.fill(black)
 
     for wall in walls:
-        pygame.draw.rect(screen, black, (wall[0] * cell_size, wall[1] * cell_size, cell_size, cell_size))
+        pygame.draw.rect(screen, blue, (wall[0] * cell_size, wall[1] * cell_size, cell_size, cell_size))
     for target in key:
-        pygame.draw.rect(screen, black, (target[0] * cell_size, target[1] * cell_size, cell_size, cell_size))
+        pygame.draw.rect(screen, red, (target[0] * cell_size, target[1] * cell_size, cell_size, cell_size))
     for box in boxes:
-        pygame.draw.rect(screen, black, (box[0] * cell_size, box[1] * cell_size, cell_size, cell_size))
-    pygame.draw.rect(screen, red, (player[0] * cell_size, player[1] * cell_size, cell_size, cell_size))
+        pygame.draw.rect(screen, green, (box[0] * cell_size, box[1] * cell_size, cell_size, cell_size))
+    pygame.draw.rect(screen, white, (player[0] * cell_size, player[1] * cell_size, cell_size, cell_size))
+
     for x in range(0, width, cell_size):
         pygame.draw.line(screen, black, (x, 0), (x, height))
     for y in range(0, height, cell_size):
         pygame.draw.line(screen, black, (0, y), (width, y))
 
+    if timerSecond < 10:
+        timerSecondStr = "0" + str(timerSecond)
+    else:
+        timerSecondStr = str(timerSecond)
+    if timerMinute < 10:
+        timerMinuteStr = "0" + str(timerMinute)
+    else:
+        timerMinuteStr = str(timerMinute)
+    timerText = font.render(timerMinuteStr + ":" + timerSecondStr, True, (255, 255, 255))
+
+    screen.blit(timerText, (width / 2 - timerText.get_width() / 2, 0))
     pygame.display.update()
 
+result = 0
 
 def check():
+    global result, current_level
+    box_in_key = False
     for box in boxes:
-        if box not in key:
-            return False
-    return True
+        if box in key:
+            box_in_key = True
+            result += 1
+            #levels[current_level][box[1]][box[0]] = 3
+            boxes.remove(box)
+            break
+
+    return box_in_key
 
 
 def move_player(hx, hy):
@@ -106,14 +148,16 @@ def move_player(hx, hy):
     player[1] = newY
 
     if check():
-        global current_level
+        global current_level, result, running
         current_level += 1
         if current_level < len(levels):
             Roon(levels[current_level])
         else:
-            print("Поздравляем!")
-            pygame.quit()
-            sys.exit()
+            if result == 8: #box quantity
+                print("Поздравляем!")
+                running = False
+                pygame.quit()
+                sys.exit()
 
 
 while running:
